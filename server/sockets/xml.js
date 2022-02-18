75ed8cc9c46722800046c7faee460fc058c78060
aezv <aezv.global@gmail.com>
2022-02-18 10:27:56 +0300

    Обработчик ошибок на сокет передачи xml

--- server/sockets/xml.js
+++ server/sockets/xml.js
@@ -2,9 +2,14 @@ const dbRAM = require('../dbRAM.js');
 
 module.exports = function (socket, rooms) {
     socket.on('xml', function (msg) {
-        if (socket.id == dbRAM.getPermission(msg.idRoom, rooms)) {
-            socket.to(msg.idRoom).emit('xml', msg.data);
-            dbRAM.setXML(msg.idRoom, msg.data, rooms);
+        try {
+            if (socket.id == dbRAM.getPermission(msg.idRoom, rooms)) {
+                socket.to(msg.idRoom).emit('xml', msg.data);
+                dbRAM.setXML(msg.idRoom, msg.data, rooms);
+            }
+        }
+        catch (e) {
+            //console.log(e);
         }
     });
 }
