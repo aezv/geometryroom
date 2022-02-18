99c10c9e4534788e649d295741df062cb5b8fb7b
aezv <aezv.global@gmail.com>
2022-02-18 10:21:10 +0300

    Обработчик ошибок на сокет чата

--- server/sockets/messages.js
+++ server/sockets/messages.js
@@ -2,7 +2,12 @@ const dbRAM = require('../dbRAM.js');
 
 module.exports = function (io, socket, rooms) {
     socket.on('message', function (msg) {
-        dbRAM.addChat(msg.idRoom, msg.userName + ': ' + msg.body, rooms);
-        io.to(msg.idRoom).emit('message', msg.userName + ': ' + msg.body);
+        try {
+            dbRAM.addChat(msg.idRoom, msg.userName + ': ' + msg.body, rooms);
+            io.to(msg.idRoom).emit('message', msg.userName + ': ' + msg.body);
+        }
+        catch (e) {
+            //console.log(e);
+        }
     });
 };
