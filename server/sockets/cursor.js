2ab7736d33543fee032bc87cd1321d83bbd847a8
aezv <aezv.global@gmail.com>
2022-02-18 10:20:17 +0300

    Обработчик ошибок на сокет курсора

--- server/sockets/cursor.js
+++ server/sockets/cursor.js
@@ -2,7 +2,12 @@ const dbRAM = require('../dbRAM.js');
 
 module.exports = function (socket, rooms) {
     socket.on('cursor', function (msg) {
-        if (socket.id == dbRAM.getPermission(msg.idRoom, rooms))
-            socket.to(msg.idRoom).emit('cursor', msg);
+        try {
+            if (socket.id == dbRAM.getPermission(msg.idRoom, rooms))
+                socket.to(msg.idRoom).emit('cursor', msg);
+        }
+        catch (e) {
+            //console.log(e);
+        }
     });
 }
