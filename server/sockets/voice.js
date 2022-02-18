c87da5a04d1412bd8d146ab1476ed471154af722
aezv <aezv.global@gmail.com>
2022-02-18 10:27:02 +0300

    Обработчик ошибок на сокет голосовой связи

--- server/sockets/voice.js
+++ server/sockets/voice.js
@@ -2,12 +2,14 @@ const dbRAM = require('../dbRAM.js');
 
 module.exports = function (socket, rooms) {
     socket.on('voice', function (msg) {
-
-        var newData = msg.data.split(";");
-        newData[0] = "data:audio/ogg;";
-        newData = newData[0] + newData[1];
-
-        socket.to(msg.idRoom).emit('voice', newData);
-
+        try {
+            var newData = msg.data.split(";");
+            newData[0] = "data:audio/ogg;";
+            newData = newData[0] + newData[1];
+            socket.to(msg.idRoom).emit('voice', newData);
+        }
+        catch (e) {
+            //console.log(e);
+        }
     });
 };
