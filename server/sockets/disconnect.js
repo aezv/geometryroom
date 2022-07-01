d5695c8796b1775dce1907103efa22e607f39a7c
aezv <aezv.global@gmail.com>
2022-07-01 16:31:22 +0300

    Правки и оптимизация сокета дисконнекта на сервере

--- server/sockets/disconnect.js
+++ server/sockets/disconnect.js
@@ -7,17 +7,14 @@ module.exports = function (io, socket, rooms) {
             var message = 'Пользователь ' + dbRAM.getUserNameByUserId(idRoom, socket.id, rooms) + ' отключился.';
             dbRAM.addChat(idRoom, message, rooms);
             io.to(idRoom).emit('message', message);
-            if (socket.id == dbRAM.getRootUser(idRoom, rooms).userId) {
-                dbRAM.setRootUser(idRoom, { userId: null, userName: null }, rooms);
+            if (socket.id == dbRAM.getRootUser(idRoom, rooms)) {
+                dbRAM.setRootUser(idRoom, rooms, null);
                 socket.to(idRoom).emit('permission', false);
             }
             else {
-                if (socket.id == dbRAM.getPermission(idRoom, rooms)) {
-                    io.to(dbRAM.getRootUser(idRoom, rooms).userId).emit('permission', true);
-                    io.to(socket.id).emit('permission', false);
-                    dbRAM.setPermission(idRoom, dbRAM.getRootUser(idRoom, rooms).userId, rooms);
-                }
-                dbRAM.delDefaultUser(idRoom, socket.id, rooms);
+                io.to(dbRAM.getRootUser(idRoom, rooms).userId).emit('permission', true);
+                io.to(socket.id).emit('permission', false);
+                dbRAM.delUser(idRoom, socket.id, rooms);
             }
             if (!dbRAM.getUseRoom(idRoom, rooms)) {
                 dbRAM.writeRoom(idRoom, rooms);
@@ -25,7 +22,7 @@ module.exports = function (io, socket, rooms) {
             }
         }
         catch (e) {
-            //console.log(e);
+            console.log(e);
         }
     });
 }
