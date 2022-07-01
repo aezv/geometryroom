5c95518f013f9ca88c8d07ad52931a7f3405f715
aezv <aezv.global@gmail.com>
2022-07-01 16:26:40 +0300

    Правки и оптимизация сокета коннекта на клиенте

--- shell/scripts/room/connect.js
+++ shell/scripts/room/connect.js
@@ -1,10 +1,7 @@
-var root = false;
-
 function loadConnect() {
-    socket.emit('connect_room', { idRoom: idRoom, roomPermission: roomPermission, rootIdRoom: rootIdRoom, userName: userName });
+    socket.emit('connect_room', { idRoom: idRoom, rootIdRoom: rootIdRoom, userName: userName });
 
     socket.on('connect_room', function (msg) {
-        root = msg;
         socket.emit('users_list', idRoom);
     });
 }
