7ebb9c381b722d3e04448ecf319366f26ed4975e
aezv <aezv.global@gmail.com>
2022-07-01 16:32:37 +0300

    Правки и оптимизация сокета курсора на сервере

--- server/sockets/cursor.js
+++ server/sockets/cursor.js
@@ -3,11 +3,11 @@ const dbRAM = require('../dbRAM.js');
 module.exports = function (socket, rooms) {
     socket.on('cursor', function (msg) {
         try {
-            if (socket.id == dbRAM.getPermission(msg.idRoom, rooms))
+            if(socket.id = dbRAM.getPermissionUserId(msg.idRoom, rooms))
                 socket.to(msg.idRoom).emit('cursor', msg);
         }
         catch (e) {
-            //console.log(e);
+            console.log(e);
         }
     });
 }
