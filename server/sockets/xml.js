68c957f28a261b84ba7575c2b6f1726d99095347
aezv <aezv.global@gmail.com>
2022-07-01 16:32:09 +0300

    Правки и оптимизация сокета xml на сервере

--- server/sockets/xml.js
+++ server/sockets/xml.js
@@ -3,13 +3,13 @@ const dbRAM = require('../dbRAM.js');
 module.exports = function (socket, rooms) {
     socket.on('xml', function (msg) {
         try {
-            if (socket.id == dbRAM.getPermission(msg.idRoom, rooms)) {
+            if (socket.id == dbRAM.getPermissionUserId(msg.idRoom, rooms)) {
                 socket.to(msg.idRoom).emit('xml', msg.data);
                 dbRAM.setXML(msg.idRoom, msg.data, rooms);
             }
         }
         catch (e) {
-            //console.log(e);
+            console.log(e);
         }
     });
 }
