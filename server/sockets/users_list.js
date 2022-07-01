31d45c83199892b89ae006c3294f526ffe03fc8f
aezv <aezv.global@gmail.com>
2022-07-01 16:33:29 +0300

    Правки и оптимизация сокета списка пользователей на сервере

--- server/sockets/users_list.js
+++ server/sockets/users_list.js
@@ -3,10 +3,10 @@ const dbRAM = require('../dbRAM.js');
 module.exports = function (io, socket, rooms) {
     try {
         socket.on('users_list', function (msg) {
-            io.to(msg).emit('users_list', dbRAM.getAllUsers(msg, rooms));
+            io.to(msg).emit('users_list', dbRAM.getUsers(msg, rooms));
         });
     }
     catch (e) {
-        //console.log(e);
+        console.log(e);
     }
 };
