dea9afd35d912f60bcd5c8fa42b6246f5bd278b9
aezv <aezv.global@gmail.com>
2022-07-01 16:26:14 +0300

    Правки и оптимизация сокета коннекта на сервере

--- server/sockets/connect.js
+++ server/sockets/connect.js
@@ -1,45 +1,54 @@
 const dbRAM = require('../dbRAM.js');
 
+function localAddUser(io, socket, rooms, msg) {
+    dbRAM.addUser(msg.idRoom, { userId: socket.id, userName: msg.userName }, rooms);
+    io.to(socket.id).emit('connect_room', null);
+}
+
+function localSetRootUser(io, socket, rooms, msg) {
+    dbRAM.setRootUser(msg.idRoom, rooms, socket.id);
+    dbRAM.setPermissionUserId(msg.idRoom, rooms, socket.id);
+    dbRAM.addUser(msg.idRoom, { userId: socket.id, userName: msg.userName }, rooms);
+    io.to(socket.id).emit('connect_room', null);
+}
+
 module.exports = function (io, socket, rooms, stats) {
     socket.on('connect_room', function (msg) {
         try {
-            stats.users++;
             socket.join(msg.idRoom);
-            if (!dbRAM.searchRoom(msg.idRoom, rooms))
+            if (!dbRAM.searchRoom(msg.idRoom, rooms)){
                 dbRAM.readRoom(msg.idRoom, rooms);
-            if (msg.roomPermission == dbRAM.getRoomPermission(msg.idRoom, rooms) && msg.roomPermission == 'restricted') {
+            }
+
+            if (dbRAM.getRoomPermission(msg.idRoom, rooms) == 'free') {
+                localAddUser(io, socket, rooms, msg);
+            }
+            else if (dbRAM.getRoomPermission(msg.idRoom, rooms) == 'restricted') {
                 if (msg.rootIdRoom == dbRAM.getRootIdRoom(msg.idRoom, rooms)) {
-                    dbRAM.setRootUser(msg.idRoom, { userId: socket.id, userName: msg.userName }, rooms);
-                    io.to(socket.id).emit('connect_room', true);
-                    io.to(socket.id).emit('permission', true);
-                    dbRAM.setPermission(msg.idRoom, socket.id, rooms);
+                    localSetRootUser(io, socket, rooms, msg);
                 }
                 else {
-                    dbRAM.addDefaultUser(msg.idRoom, { userId: socket.id, userName: msg.userName }, rooms);
-                    io.to(socket.id).emit('connect_room', false);
-                    io.to(socket.id).emit('permission', false);
+                    localAddUser(io, socket, rooms, msg);
                 }
             }
-            else if (msg.roomPermission == dbRAM.getRoomPermission(msg.idRoom, rooms) && msg.roomPermission == 'free') {
-                dbRAM.addDefaultUser(msg.idRoom, { userId: socket.id, userName: msg.userName }, rooms);
-                io.to(socket.id).emit('connect_room', false);
-                io.to(socket.id).emit('permission', false);
-            }
-            else {
-                console.log('Ошибка в коннекте 1 = ' + msg.roomPermission + ', 2 = ' + dbRAM.getRoomPermission(msg.idRoom, rooms));
-                console.log(msg);
-            }
+            else
+                throw 'Room: ' + msg.idRoom + ', Error: Unknown room permissions';
+
             var xml = dbRAM.getXML(msg.idRoom, rooms);
             if (xml)
                 io.to(socket.id).emit('xml', xml);
+
             var chat = dbRAM.getChat(msg.idRoom, rooms);
             if (chat.length)
                 io.to(socket.id).emit('message', chat);
             dbRAM.addChat(msg.idRoom, 'Пользователь ' + msg.userName + ' присоединился.', rooms);
+            
             io.to(msg.idRoom).emit('message', 'Пользователь ' + msg.userName + ' присоединился.');
+
+            stats.users++;
         }
         catch (e) {
-            //console.log(e);
+            console.log(e);
         }
     });
 };
